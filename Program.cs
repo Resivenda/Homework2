f84ccf350fd19b46ad9c3cf3c746bcfb17c559fd
Alenae <alena_trsenkov@list.ru>
2023-10-10 23:56:58 +0300

    all

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadInt(string msg)
+{
+    Console.WriteLine(msg); 
+    string numb = Console.ReadLine();
+    int number; 
+    number = int.Parse(numb);
+    return number; 
+} 
+int number= ReadInt("введите трехзначное число");
+if (number < 1000 & number > 99)
+{
+    int mediumDigit = (number %100-number%10)/10 ; 
+    Console.WriteLine("mediumDigit=" + mediumDigit); 
+
+}
+else Console.WriteLine("mediumDigit not three digits");
